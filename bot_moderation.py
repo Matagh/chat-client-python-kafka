d5515a88b6fb6d628d77470158098dbd1af5e222
Louise Lassalle <l.lassalle02@gmail.com>
2022-06-12 16:01:53 +0200

    Update bot
    new Dataframe with username as value for kafka output

--- bot_moderation.py
+++ bot_moderation.py
@@ -1,11 +1,14 @@
+
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import window
+from pyspark.sql.functions import col
+
 
 spark = SparkSession \
           .builder \
           .appName("BOT_MODERATION") \
           .getOrCreate()
-
+#lie spark au kafka stream sur le topic associé à la modération
 message_received = spark\
       .readStream \
       .format("kafka") \
@@ -15,6 +18,11 @@ message_received = spark\
       .load()
 #verifie le nombre de message par user en 5 sec
 message_treatment = message_received.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
-ds = message_treatment.writeStream.format("kafka").option("kafka.bootstrap.servers", "localhost:9092").option("checkpointLocation", "chat_moderation_response").start()
-
+#Check DataFrame structure
+f= open("/Users/louise/Desktop/FORMATION/Alternance/spark/chat-client-python-kafka/test.txt", "a")
+f.write(str(message_received))
+f.close()
+#send back to a kafka topic in new dataFrame structured with username as value
+test_data = message_received.withColumn("value",col("key").cast("string"))
+ds = test_data.writeStream.format("kafka").option("kafka.bootstrap.servers", "localhost:9092").option("topic", "chat_moderation_response").option("checkpointLocation", "/Users/louise/Desktop/FORMATION/Alternance/spark/chat-client-python-kafka/tmp").start()
 ds.awaitTermination()
